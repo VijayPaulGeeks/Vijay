b96c85978af92b4cfe43eeb4a9a8ebdb303bbf91
VijayPaulGeeks <crissvijay.8@gmail.com>
2022-02-15 13:23:00 +0530

    message

---
+++ src/test/java/org/vijaytest/Vijay1.java
@@ -0,0 +1,58 @@
+package org.vijaytest;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Vijay1 {
+public static void main(String[] args) throws IOException {
+	File file = new File("C:\\Users\\Vijay criss\\eclipse-workspace\\Vijay\\xl\\Maven01.xlsx");
+	FileInputStream stream =new FileInputStream(file);
+	Workbook work = new XSSFWorkbook(stream);
+	Sheet sheet =work.getSheet("Table");
+	for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+		Row r =sheet.getRow(i);
+		for (int j = 0; j < r.getPhysicalNumberOfCells(); j++) {
+			Cell c=r.getCell(j);
+			System.out.println(c);
+			
+			CellType celltype = c.getCellType();
+			
+			switch (celltype) {
+			
+			case STRING:
+				String data = c.getStringCellValue();
+				System.out.println(data);
+				break;
+			
+			case NUMERIC:
+				double d = c.getNumericCellValue();
+				BigDecimal b = BigDecimal.valueOf(d);
+				String name = b.toString();
+				System.out.println(name);
+				break;
+				
+				default:
+					break;
+				}
+			
+			
+			
+			}
+			
+	}
+		
+		
+		
+	}
+}
+
